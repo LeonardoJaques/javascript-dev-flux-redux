45942e3b5a8adff2b2a7bac3acf9fb0cbb376f42
Leonardo <jaques.projetos@outlook.com>
2018-10-27 17:44:42 -0300

    input undo buntton function to works, anda complete the task of todolist.

--- fluxTodoList/src/flux/ReduceStore.js
+++ fluxTodoList/src/flux/ReduceStore.js
@@ -3,7 +3,7 @@ export class ReduceStore extends Store{
 
     constructor(dispatcher){
             super(dispatcher);
-
+            this.__history = [];
     }
 
     reduce(state, action){
@@ -13,8 +13,16 @@ export class ReduceStore extends Store{
     __onDispatch(action){
         const newState = this.reduce(this.__state, action);
         if (newState !== this.__state){
+            this.__history.push(this.__state);
             this.__state = newState;
             this.__emitChange();
         }
+    }
+
+    revertLastState(){
+        if (this.__history.length > 0){
+            this.__state = this.__history.pop();
+        }
+        this.__emitChange();
     }
 }
